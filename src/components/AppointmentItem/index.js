a45d2001e80bc5b16ae8462f4d32017d98175906
Manikalyan123 <kalyan.boddepalli1999@gmail.com>
2022-12-29 16:58:04 +0000

    solution

--- src/components/AppointmentItem/index.js
+++ src/components/AppointmentItem/index.js
@@ -1,11 +1,18 @@
 // Write your code here
 import './index.css'
 
-const AppointmentItem = () => {
+const AppointmentItem = props => {
+  const {eachAppointment} = props
+  const {title, date, isStarred} = eachAppointment
+  const Star = isStarred
+    ? 'https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png'
+
   return (
     <li className="list-order">
-      <h1>Dentist</h1>
-      <p>Date:22-12-2022</p>
+      <h1 className="appName">{title}</h1>
+      <p className="appDate">{date}</p>
+      <img src={Star} alt="star" />
     </li>
   )
 }
--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -1,34 +1,50 @@
 // Write your code here
 import {Component} from 'react'
 import './index.css'
+import {v4 as uuid} from 'uuid'
 import AppointmentItem from '../AppointmentItem'
 
-const AppointmentList = []
-
 class Appointments extends Component {
-  state = {appointment: AppointmentList, Title: '', Date: ''}
+  state = {appointment: [], Title: '', date: ''}
 
   onTitleChange = event => {
     this.setState({Title: event.target.value})
   }
 
+  onSubmitClick = () => {
+    const {Title, date} = this.state
+    const newAppointment = {
+      id: uuid(),
+      Title,
+      date,
+      isStarred: false,
+    }
+    this.setState(prevState => ({
+      appointment: [...prevState.appointment, newAppointment],
+      Title: '',
+      date: '',
+    }))
+  }
+
   render() {
-    const {Title, Date} = this.state
+    const {Title, appointment} = this.state
     return (
       <div className="gradient-Cont">
         <div className="white-cont">
           <div className="add-appointment-cont">
             <h1>Add Appointment</h1>
-            <form className="form-cont">
+            <form className="form-cont" onSubmit={this.onSubmitClick}>
+              <label htmlFor="title">TITLE</label>
               <input
+                id="title"
                 className="input"
                 value={Title}
                 onChange={this.onTitleChange}
                 placeholder="Title"
               />
               <br />
-
-              <input type="date" className="date-input" />
+              <label htmlFor="date">DATE</label>
+              <input id="date" type="date" className="date-input" />
               <br />
               <button type="submit" className="button">
                 Add
@@ -38,7 +54,9 @@ class Appointments extends Component {
           <hr />
           <h1>Appointments</h1>
           <ul className="unorder-list">
-            <AppointmentItem onClickSubmit={this.onSubmit} />
+            {appointment.map(each => (
+              <AppointmentItem eachAppointment={each} key={each.id} />
+            ))}
           </ul>
         </div>
       </div>
